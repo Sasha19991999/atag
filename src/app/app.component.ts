ca5444cf379b414728924175e53dad2b1c6ee305
Antonio Angel <antonio.cruzado@softcrits.es>
2023-03-12 21:05:14 +0100

    Full-restructure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit, AfterViewInit, Input, ViewChild, ElementRef } from '@angular/core';
-import * as THREE from "three";
-import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
-import { interval } from 'rxjs';
-import { InteractionService } from './services/interaction.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit  {
+export class AppComponent {
 
-  public selectedInfo = 0;
-
-  constructor(private interactionService: InteractionService) { }
-
-  ngOnInit(): void {
-    this.interactionService.selectedInfo$.subscribe(value => {
-      this.selectedInfo = value;
-    })
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ObjectViewerComponent } from './object-viewer/object-viewer.component';
+import { InteractiveViewerComponent } from './pages/interactive-viewer/interactive-viewer.component';
+import { ProductFeaturesComponent } from './pages/product-features/product-features.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ObjectViewerComponent
+    ObjectViewerComponent,
+    InteractiveViewerComponent,
+    ProductFeaturesComponent
   ],
   imports: [
     BrowserModule,
